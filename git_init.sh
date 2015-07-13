5f578b1a5e9e4dc5ed460cb5e547b30f7c60306e
sundajiang <450160193@qq.com>
2015-07-13 14:42:11 +0800

    添加了git——push脚本文件

--- git_init.sh
+++ git_init.sh
@@ -3,6 +3,6 @@
 git init
 git add -A
 git commit -m "面试问题代码初步提交"
-
+#添加远程主机$ git remote add <主机名> <网址>
 git remote add origin git@github.com:sundajiang/interview.git
 git push -u origin master
---
+++ git_push.sh
@@ -0,0 +1,4 @@
+
+git add -A
+git commit -m "添加了git——push脚本文件"
+git push -u origin master
